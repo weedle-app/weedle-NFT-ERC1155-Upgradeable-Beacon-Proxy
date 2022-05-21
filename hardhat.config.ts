028f77055d04d44a7fd6040362bcb2b9b72f899a
Doyin Olarewaju <youngenterpreneur11@gmail.com>
2022-05-20 23:58:34 -0600

    Comment out un-used config from hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import config from "config";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
